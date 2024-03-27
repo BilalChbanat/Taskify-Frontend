120db70c83c1b9b5d259c0261e6305e4d9ba694c
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-03-27 11:45:32 +0000

    featchi all tasks

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,29 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+  <header class="text-gray-600 body-font">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <RouterLink to="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+
+        <span class="ml-3 text-xl"><span class="text-blue-500"><b>Task</b></span><span class="text-purple-800"> <b>ify
+            </b></span></span>
+      </RouterLink>
+      <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+        <RouterLink to="/about" class="mr-5 hover:text-purple-700">About Us</RouterLink>
+        <RouterLink to="/tasks" class="mr-5 hover:text-purple-700">My Tasks</RouterLink>
+
       </nav>
+      <!-- <button
+        class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+          <path d="M5 12h14M12 5l7 7-7 7"></path>
+        </svg>
+      </button> -->
     </div>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/views/tasks/View.vue
@@ -0,0 +1,64 @@
+<template>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+                <div class="flex items-center justify-between">
+                    <button wire:click="create()"
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-3">Create New
+                        Post</button>
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                        Manage My tasks
+                    </h2>
+                </div>
+                <table class="table-fixed w-full">
+                    <thead>
+                        <tr class="bg-gray-100">
+                            <th class="px-4 py-2 w-20">No.</th>
+                            <th class="px-4 py-2">Name</th>
+                            <th class="px-4 py-2">description</th>
+                            <th class="px-4 py-2">status</th>
+                            <th class="px-4 py-2">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="border px-4 py-2">1</td>
+                            <td class="border px-4 py-2">Title goes here</td>
+                            <td class="border px-4 py-2">Body</td>
+                            <td class="border px-4 py-2">doing</td>
+                            <td class="border px-4 py-2">
+                                <button wire:click="edit({{ $post->id }})"
+                                    class="bg-blue-400 ml-6 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Edit</button>
+                                <button wire:click="delete({{ $post->id }})"
+                                    class="bg-red-400 ml-6 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'tasks',
+    data(){
+        return {
+            tasks: []
+        }
+    },
+    mounted(){
+        this.getTasks();
+    },
+    methods:{
+        getTasks(){
+            axios.get('http://127.0.0.1:8000/api/V1/tasks').then(res => {
+                console.log(res);
+            });
+        }
+    },
+}
+</script>
