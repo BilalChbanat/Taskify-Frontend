fd11b5afb82d872c76fd7a1785169782c0057c1b
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-04-03 00:18:44 +0000

    displaying the tasks is done

--- src/views/tasks/View.vue
+++ src/views/tasks/View.vue
@@ -1,10 +1,9 @@
 <template>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
                 <div class="flex items-center justify-between">
-                    <button wire:click="create()"
+                    <button @click="create()"
                         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-3">Create New
                         Task</button>
                     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -16,49 +15,69 @@
                         <tr class="bg-gray-100">
                             <th class="px-4 py-2 w-20">No.</th>
                             <th class="px-4 py-2">Name</th>
-                            <th class="px-4 py-2">description</th>
-                            <th class="px-4 py-2">status</th>
+                            <th class="px-4 py-2">Description</th>
+                            <th class="px-4 py-2">Status</th>
                             <th class="px-4 py-2">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td class="border px-4 py-2">1</td>
-                            <td class="border px-4 py-2">Title goes here</td>
-                            <td class="border px-4 py-2">Body</td>
-                            <td class="border px-4 py-2">doing</td>
-                            <td class="border px-4 py-2">
-                                <button wire:click="edit({{ $post->id }})"
-                                    class="bg-blue-400 ml-6 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Edit</button>
-                                <button wire:click="delete({{ $post->id }})"
-                                    class="bg-red-400 ml-6 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">Delete</button>
-                            </td>
-                        </tr>
+                        <template v-if="tasks.length > 0">
+                            <tr v-for="(task, index) in tasks" :key="task.id">
+                                <td class="border px-4 py-2">{{ task.id }}</td>
+                                <td class="border px-4 py-2">{{ task.name }}</td>
+                                <td class="border px-4 py-2">{{ task.description }}</td>
+                                <td class="border px-4 py-2">{{ task.status }}</td>
+                                <td class="border px-4 py-2">
+                                    <RouterLink to="/"
+                                        class="bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+                                        Edit
+                                    </RouterLink>
+                                    <button type="button"
+                                        class="bg-red-400 ml-2 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        </template>
+                        <template v-else>
+                            <tr>
+                                <td class="border px-4 py-2" colspan="5">No tasks available</td>
+                            </tr>
+                        </template>
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
 </template>
-
 <script>
 import axios from 'axios';
 export default {
     name: 'tasks',
-    data(){
+    data() {
         return {
             tasks: []
         }
     },
-    mounted(){
+    mounted() {
         this.getTasks();
     },
-    methods:{
-        getTasks(){
-            axios.get('http://127.0.0.1:8000/api/V1/tasks').then(res => {
-                console.log(res);
-            });
+    methods: {
+        async getTasks() {
+            try {
+                const accessToken = localStorage.getItem('token');
+                const response = await axios.get('http://127.0.0.1:8000/api/V1/tasks', {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`
+                    }
+                });
+                this.tasks = response.data.data;
+                console.log(response);
+
+            } catch (error) {
+                console.error('Error fetching tasks:', error);
+            }
         }
-    },
+    }
 }
 </script>
